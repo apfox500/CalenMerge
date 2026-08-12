4efe665ef827c7fd8bd1eae7c89ca65c52b675a2
Andrew Fox <apfox500@gmail.com>
2026-08-12 14:09:54 -0600

    created correct files

---
+++ src/calenmerge/app/api/merge/route.ts
@@ -0,0 +1,9 @@
+
+export async function GET(request: Request){
+    // figure out who was in the exclude field
+    
+    // call function to get merged calendar data
+
+    // return ical link with the merged calendar data
+    return new Response();
+}
